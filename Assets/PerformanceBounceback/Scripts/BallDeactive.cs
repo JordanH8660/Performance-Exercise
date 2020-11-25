80c5c6f23b58311e39ee931057cf193f20ecb41c
Jordan8660 <jordan.hogan@lethbridgecollege.ca>
2020-11-24 18:42:33 -0700

    Project Finished

---
+++ Assets/PerformanceBounceback/Scripts/BallDeactive.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallDeactive : MonoBehaviour
+{
+
+    private GameManager gameManager;
+
+    private void Start()
+    {
+      
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Ground")
+        {
+            gameObject.SetActive(false);
+        }
+
+        if (collision.gameObject.name == "Trampoline") 
+        {
+           
+        }
+    }
+
+}
---
+++ Assets/PerformanceBounceback/Scripts/BallSpawner.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawner : MonoBehaviour {
+
+    public static BallSpawner SharedInstance;
+    public List<GameObject> pooledObjects;
+    public GameObject objectToPool; //the prefab of the object in the object pool
+    public int amountToPool;
+    public static int ballPoolNum;
+
+    private float cooldown;
+    private float cooldownLength = 0.5f;
+
+    private void Awake()
+    {
+        SharedInstance = this;
+    }
+    private void Start()
+    {
+        pooledObjects = new List<GameObject>(); //creates the pool of objects
+        for (int i = 0; i < amountToPool; i++)
+        {
+            GameObject obj = (GameObject)Instantiate(objectToPool);
+            obj.SetActive(false);
+            pooledObjects.Add(obj);
+        }
+        InvokeRepeating("SpawnBall", 0, 0.4f);
+    }
+    
+   	public GameObject GetPooledObject()
+    {
+        for (int i = 0; i < pooledObjects.Count; i++) //iterates through the pooled objects list
+        {
+            if (!pooledObjects[i].activeInHierarchy)
+            {
+                return pooledObjects[i];
+            }
+        } 
+        return null;
+    }
+
+    void SpawnBall()
+    {
+        GameObject selectedBall = BallSpawner.SharedInstance.GetPooledObject();
+
+        if (selectedBall != null)
+        {
+            selectedBall.SetActive(true);
+            Rigidbody selectedRigidBody = selectedBall.GetComponent<Rigidbody>();
+            selectedBall.transform.position = transform.position;
+        }
+        ballPoolNum++;
+        if (ballPoolNum >= amountToPool)
+        {
+            ballPoolNum = 0;
+        }
+    }
+}
